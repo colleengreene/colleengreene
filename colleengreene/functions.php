14d38cd354e9fa7b44616bd6bfd0a82652d5f8e7
Colleen Greene <colleen.e.greene@gmail.com>
2015-11-10 13:47:10 -0800

    Applied eNews Signup box to Mexico Gen tag
    Using a conditional tag, when in that particular tag.

--- colleengreene/functions.php
+++ colleengreene/functions.php
@@ -50,11 +50,11 @@ function sp_share_buttons_above_post( $content = '' ) {
 
 /* ****START HISPANIC NEWSLETTER SIGNUP BOX AFTER POST**** */
 function enews_signup_box() {
-if ( is_single() && in_category('Mexican genealogy')) { ?>
+if ( is_single() && has_tag('Mexican genealogy')) { ?>
 
 <!-- Start Sign UP Box -->
-<div class="signup-box"><span class="signup-box-heading">Hispanic Genealogy Tips &amp; News</span>
-Interested in Hispanic genealogy and history?
+<div class="signup-box"><span class="signup-box-heading">Hispanic Genealogy Tips &amp; News</span><br />
+Interested in Hispanic genealogy and history?<br />
 <strong><a href="http://colleengreene.us12.list-manage1.com/subscribe?u=566e975d594e41e054953fe1b&amp;id=baa5909068">SIGN UP NOW</a><strong> to get my FREE email newsletter, <strong><em>Hispanic Heritage Happenings</em></strong>, delivered to your email inbox the 1st and 15th of every month!</strong></strong></div>
 <!-- End Sign Up-->
 
