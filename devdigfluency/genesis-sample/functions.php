4640889e2a03968e01ee70e4c7f8a32dd47cde92
Colleen <colleen.e.greene@gmail.com>
2021-12-04 15:06:54 -0800

    Added Jetpack Share Buttons Above Posts too
    Along with at the bottom.

--- devdigfluency/genesis-sample/functions.php
+++ devdigfluency/genesis-sample/functions.php
@@ -291,3 +291,20 @@ function cg_top_wrap_widgets() {
         'after' => '</div>',));
     }
 add_action( 'genesis_after_header', 'cg_top_wrap_widgets' );
+
+
+//* Add Jetpack share buttons above post
+remove_filter( 'the_content', 'sharing_display', 19 );
+remove_filter( 'the_excerpt', 'sharing_display', 19 );
+
+add_filter( 'the_content', 'sp_share_buttons_above_post', 19 );
+add_filter( 'the_excerpt', 'sp_share_buttons_above_post', 19 );
+
+function sp_share_buttons_above_post( $content = '' ) {
+	if ( function_exists( 'sharing_display' ) ) {
+		return sharing_display() . $content;
+	}
+	else {
+		return $content;
+	}
+}
