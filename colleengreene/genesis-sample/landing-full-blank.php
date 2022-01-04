6e294cab45cb61d3f010e67d83358ba6217e58fe
Colleen <colleen.e.greene@gmail.com>
2022-01-03 20:48:07 -0800

    Continue setting up new template
    Remove footer
    Remove custom sidebar Top Wrap

--- colleengreene/genesis-sample/landing-full-blank.php
+++ colleengreene/genesis-sample/landing-full-blank.php
@@ -13,11 +13,14 @@ remove_theme_support( 'genesis-menus' );
 // This removes breadcrumbs
 remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 
-// This removes the sidebar
+// This removes the primary sidebar
 remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
 
+// This removes my custom Top Wrap sidebar
+
+
 // Removes the site footer
-remove_theme_support( 'genesis-footer-widgets' );
+remove_action( 'genesis_after_header', 'cg_top_wrap_widgets' );
 
 // This removes the site footer elements
 remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
