0975f81da72ed5a57b35462594205a07b6190797
Colleen <colleen.e.greene@gmail.com>
2022-01-03 20:21:04 -0800

    initial rounds of customizing, to strip the page blank.

--- colleengreene/genesis-sample/landing-full-blank.php
+++ colleengreene/genesis-sample/landing-full-blank.php
@@ -4,5 +4,25 @@
 * Description: Template for a custom landing page page that is full width and blank with default elements.
 */
 
+// this removes the site header
+remove_action( 'genesis_header', 'genesis_do_header' );
+
+// This removes the site navigation
+remove_theme_support( 'genesis-menus' );
+
+// This removes breadcrumbs
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+// This removes the sidebar
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+
+// Removes the site footer
+remove_theme_support( 'genesis-footer-widgets' );
+
+// This removes the site footer elements
+remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+
 genesis();
 
